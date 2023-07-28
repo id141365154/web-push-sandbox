99ee930e46318f4b9bab51adcb9ac189e1fc43b2
ku <ku@kode.ru>
2023-07-28 12:19:59 +0200

    add request

---
+++ src/send-push-request.sh
@@ -0,0 +1,8 @@
+web-push send-notification  \
+  --endpoint=https://fcm.googleapis.com/fcm/send/dahYPF3MN5c:APA91bHWdx-za5fp2Kw2TcZEMExO29cWEK3rSkp0iQQIiJuvWuwJDMcX0F3TNoAaTVt8_QD9bwPZYsnj7LFJZ9-DqzxrkGAGBeKQwlbHVo_zZwDFYaqcLKKFdvXP-hZeWsyMe0o8_KUM \
+  --key=BC9HD2yR3QEk5e2-4upc4U2LAemt6A3ndrXOBgOdIrOmd1h9FtyJOWK9u2gWTRZsbSQOma_8fd3--tMSJ4SUZYg \
+  --auth=75AudVs5eoCtuT9XqXO0Hw \
+  --vapid-subject=mailto:ku@kode.ru \
+  --vapid-pubkey=BMf6YbvbCiHlLTiqJId5Ub23AJIq2UXOlf-OOIeXOW6ttipRa3dilKmjdNX6eW_Jxsx_qCQfq3XbpU2hmbKjaEM \
+  --vapid-pvtkey=ji4i6WCc7CgQ2JDIqkcGgFJcwK0wuFxxtyOunNZJ0iw \
+  --payload=Helloworlds!
